5bc92c94182366beb2c4b05d21d245c105abd102
JamesWilson <wesstation2@gmail.com>
2018-08-13 12:03:04 +0800

    新增首頁

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Shop from '../Shop/Shop'
+
+class Home extends React.Component {
+    render () {
+        <div>
+            <Header/>
+            <Shop/>
+        </div>
+    }
+}
+
+export default Home
