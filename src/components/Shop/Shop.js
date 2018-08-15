e24bb1702002dfa70b959b00afc7f423f6e6666c
JamesWilson <wesstation2@gmail.com>
2018-08-15 15:01:05 +0800

    更新商店的

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -36,6 +36,61 @@ class Shop extends React.Component {
         this.setState({
             cart: items
         })
+    }
 
+    addToCart(key) {
+        var item = this.state.items.find(itm => itm.key === key);
+        var newCart = [...this.state.cart];
+        newCart.push(item);
+        
+
+        var newCartCount = Object.assign({}, this.state.cartCount);
+        var newCount = (newCartCount[key] || 0) + 1;
+        newCartCount[key] = newCount;
+        
+        this.setState({
+            cart: newCart,
+            cartCount: newCartCount
+        });
+
+        addToDatabaseCart(key, newCount);
+    }
+
+    handleSearch(event) {
+        var searchText = event.target.value.toLowerCase();
+        var matched = fakeData.filter(item => item.category.toLowerCase().includes(searchText) || item.name.toLowerCase().includes(searchText));
+        var first10 = matched.slice(0, 10);
+        this.setState({
+            items:first10
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="search-container">
+                    <input type="text" className="search-input" onKeyUp={this.handleSearch} placeholder="type here to search" />
+                    <Link to="/review">
+                        <span className="cart-count">{this.state.cart.length}</span>
+                    </Link>
+                </div>
+                <div className="shop-container">
+                    <div className="items-container">
+                        {this.state.items.map(item => <ShopItem key={item.key} item={item} addToCart={this.addToCart}></ShopItem> )}
+                    </div>
+                    <div className="cart-container">
+                        <Cart cart={this.state.cart}>
+                            <Link to="/review">
+                                <button>
+                                    <span>Review your order</span>
+                                </button>
+                            </Link>
+                        </Cart>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
+
+export default Shop;
